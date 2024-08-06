0e592113faceaf91151e739ecd1029497fc68935
Viniciusmagal <162904345+Viniciusmagal@users.noreply.github.com>
2024-08-05 22:28:02 -0300

    Add files via upload

---
+++ Avaliação 4º Bim/Avaliação 4º Bim/JS/function.js
@@ -0,0 +1,23 @@
+
+var capturar = "";
+function capturado() {
+
+capturar = document.getElementById('nome').value;
+
+
+document.getElementById('valorzada').innerHTML = capturar;
+
+
+
+alert(document.getElementById('valorzada').innerHTML = capturar);
+
+    
+}
+
+
+
+
+
+
+
+
